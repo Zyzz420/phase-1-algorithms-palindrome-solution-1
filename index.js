3a25116b6ef79a031b5735d132aaa8fe0985fa5d
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-07-16 17:49:29 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 function isPalindrome(word) {
-  // Write your algorithm here
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -23,3 +14,18 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord
+}
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+
+  if(word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
